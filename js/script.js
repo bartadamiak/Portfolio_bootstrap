23915cdf60c9d14d2c24fe01058f8ab85b64b739
bartadamiak <bart.adamiak@gmail.com>
2019-05-14 17:41:38 +0200

    Animation

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const circle = document.querySelector('.circle');
+const logos = document.querySelectorAll('.circle i');
+console.log(logos)
+
+circle.addEventListener('mouseenter', function(){
+    this.classList.toggle('bg-light');
+    for (let i = 0; i < logos.length; i++) {
+        logos[i].classList.toggle('text-light');
+
+    }
+});
+
+circle.addEventListener('mouseleave', function(){
+    this.classList.toggle('bg-light');
+    for (let i = 0; i < logos.length; i++) {
+        logos[i].classList.toggle('text-light');
+
+    }
+})
